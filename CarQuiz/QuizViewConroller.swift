eed37e752cd3e8d57ddfd7d78f27d1e6a5c2e0fe
dai-guu <89032169+dai-guu@users.noreply.github.com>
2022-02-20 19:13:51 +0900

    Initial Commit

---
+++ CarQuiz/QuizViewConroller.swift
@@ -0,0 +1,29 @@
+//
+//  QuizViewConrollerViewController.swift
+//  CarQuiz
+//
+//  Created by おがわひさと on 2022/02/19.
+//
+
+import UIKit
+
+class QuizViewConrollerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CarQuiz/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CarQuiz
+//
+//  Created by おがわひさと on 2022/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
